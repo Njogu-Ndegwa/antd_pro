8d52ebd66e14cbfe3563aa230353fae0f606551d
unknown <dennis@ciphercom.co.ke>
2020-05-04 13:34:58 +0300

    Initial commit

---
+++ config/defaultSettings.js
@@ -0,0 +1,18 @@
+export default {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: "#52C41A",
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: true,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+};
+
